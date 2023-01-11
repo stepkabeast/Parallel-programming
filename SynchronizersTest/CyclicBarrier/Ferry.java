42fbd6a2b9a546cf01bb0e9e721d64f61459e9df
Kovalev Stepan <stepak038@gmail.com>
2023-01-11 18:23:33 +0800

    Add files via upload

---
+++ SynchronizersTest/CyclicBarrier/Ferry.java
@@ -0,0 +1,55 @@
+import java.util.concurrent.CyclicBarrier;
+/**
+ * @author Stepan Kovalev
+ * Рассмотрим следующий пример.
+ * Существует паромная переправа. Паром может переправлять одновременно по три автомобиля.
+ * Чтобы не гонять паром лишний раз, нужно отправлять его, когда у переправы соберется минимум три автомобиля.
+ *
+ *
+ * Когда три потока достигают метода await(), барьерное действие запускается,
+ * и паром переправляет три автомобиля из скопившихся. После этого начинается новый цикл.*/
+public class Ferry {
+    private static final CyclicBarrier BARRIER = new CyclicBarrier(3, new FerryBoat());
+    //Инициализируем барьер на три потока и таском, который будет выполняться, когда
+    //у барьера соберется три потока. После этого, они будут освобождены.
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 9; i++) {
+            new Thread(new Car(i)).start();
+            Thread.sleep(400);
+        }
+    }
+
+    //Таск, который будет выполняться при достижении сторонами барьера
+    public static class FerryBoat implements Runnable {
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(500);
+                System.out.println("Паром переправил автомобили!");
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+
+    //Стороны, которые будут достигать барьера
+    public static class Car implements Runnable {
+        private int carNumber;
+
+        public Car(int carNumber) {
+            this.carNumber = carNumber;
+        }
+
+        @Override
+        public void run() {
+            try {
+                System.out.printf("Автомобиль №%d подъехал к паромной переправе.\n", carNumber);
+                //Для указания потоку о том что он достиг барьера, нужно вызвать метод await()
+                //После этого данный поток блокируется, и ждет пока остальные стороны достигнут барьера
+                BARRIER.await();
+                System.out.printf("Автомобиль №%d продолжил движение.\n", carNumber);
+            } catch (Exception e) {
+            }
+        }
+    }
+}
